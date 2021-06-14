97ed4c1ad0436e6a06337ce1b9be29c70c9b7ad4
BanB <banb3515@outlook.kr>
2021-06-14 18:42:37 +0900

    Initial Commit

---
+++ HeyMask/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HeyMask
+//
+//  Created by BanB on 2021/06/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
